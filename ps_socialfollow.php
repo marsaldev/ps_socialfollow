ffa1c3ad947cc045939f50d410f47793d1ca7793
Maxime Biloé <maxime.biloe@gmail.com>
2017-02-01 14:17:47 +0100

    // Version updated to v2.0.0

--- ps_socialfollow.php
+++ ps_socialfollow.php
@@ -38,7 +38,7 @@ class Ps_Socialfollow extends Module implements WidgetInterface
     {
         $this->name = 'ps_socialfollow';
         $this->author = 'PrestaShop';
-        $this->version = '1.0.4';
+        $this->version = '2.0.0';
 
         $this->bootstrap = true;
         parent::__construct();
